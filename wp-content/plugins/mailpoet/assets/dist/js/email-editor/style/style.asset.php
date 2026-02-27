098d2f72868a3b810a2d9d105c72c9ff70fb3f2b
joyaeljose-yatnam <joyael.jose@yatnam.com>
2026-02-27 16:45:12 +0530

    Added php files

---
+++ wp-content/plugins/mailpoet/assets/dist/js/email-editor/style/style.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+if (!defined('ABSPATH')) exit;
+
+ return array('version' => 'cfd2c1d1ab107e9e75bb');
---
+++ wp-content/plugins/mailpoet/vendor/autoload_packages.php
@@ -0,0 +1,6 @@
+<?php
+namespace Automattic\Jetpack\Autoloader\jp86f79c84b4c9e16dbd5ea94fdbf12710\al5_0_8;
+if (!defined('ABSPATH')) exit;
+ // phpcs:ignore
+require_once __DIR__ . '/jetpack-autoloader/class-autoloader.php';
+Autoloader::init();
